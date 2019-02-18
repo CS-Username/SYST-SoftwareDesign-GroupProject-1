1e905264f89067cac13053e8015dc65966af64fe
Cory Salmon <salmocor@sheridancollege.ca>
2019-02-17 22:02:06 -0500

    Merging with master

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -12,7 +12,7 @@ package ca.sheridancollege.project;
 public abstract class Player 
 {
     private String playerID; //the unique ID for this player
-    
+
     /**
      * A constructor that allows you to set the player's unique ID
      * @param name the unique ID to assign to this player.
@@ -21,12 +21,12 @@ public abstract class Player
     {
         playerID= name;
     }
-    
+
     /**
      * @return the playerID
      */
     public String getPlayerID() 
-    
+    {
         return playerID;
     }
 
@@ -38,11 +38,11 @@ public abstract class Player
     {
         playerID = givenID;
     }
-    
+
     /**
      * The method to be instantiated when you subclass the Player class
      * with your specific type of Player and filled in with logic to play your game.
      */
     public abstract void play();
-    
+
 }
