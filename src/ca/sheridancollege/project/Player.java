59348d3df0cd26f9b7dd7ab42aee20214ad37570
asher117 <47724858+asher117@users.noreply.github.com>
2019-02-17 20:07:41 -0500

    Update Player.java

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -26,7 +26,7 @@ public abstract class Player
      * @return the playerID
      */
     public String getPlayerID() 
-    {
+    
         return playerID;
     }
 
@@ -41,44 +41,6 @@ public abstract class Player
     
     /**
      * The method to be instantiated when you subclass the Player class
-     * with your specific type of Player and filled in with logic to play your game.
-     */
-    public abstract void play();
-    
-}
-
-public abstract class PlayerTwo 
-{
-    private String playerTwoID; //the unique ID for this player
-    
-    /**
-     * A constructor that allows you to set the player's unique ID
-     * @param name the unique ID to assign to this player.
-     */
-    public PlayerTwo(String name)
-    {
-        playerTwoID= name;
-    }
-    
-    /**
-     * @return the playerTwoID
-     */
-    public String getPlayerTwoID() 
-    {
-        return playerTwoID;
-    }
-
-    /**
-     * Ensure that the playerTwoID is unique
-     * @param givenID the playerTwoID to set
-     */
-    public void setPlayerTwoID(String givenID) 
-    {
-        playerTwoID = givenID;
-    }
-    
-    /**
-     * The method to be instantiated when you subclass the PlayerTwo class
      * with your specific type of Player and filled in with logic to play your game.
      */
     public abstract void play();
