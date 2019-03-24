a2de31cbe0c6fcacf6e1ecc3fb73892674788c89
Nawaphan Chayopathum <chayopat@sheridanc.on.ca>
2019-03-24 18:36:01 -0400

    2nd War Game

---
+++ src/ca/sheridancollege/project/WarPlayer.java
@@ -0,0 +1,60 @@
+/*
+   
+*/
+
+package ca.sheridancollege.project;
+/**
+ * 
+ * @author Cory
+ * @modified by Nawaphan Chayopathum(Jan)
+ */
+public class WarPlayer extends Player{
+
+    private GroupOfCards playerHand;
+    private GroupOfCards playedHand;
+    
+    public WarPlayer(String name) {
+        super(name);
+        playerHand = new GroupOfCards();
+        playedHand = new GroupOfCards();
+    }
+
+//    @Override
+//    public void play() {
+//    }
+
+    public Card playCard() {
+        if (playerHand.getSize() > 0) {
+            return playerHand.getMostRecentCard();
+        } else if (playerHand.getSize() == 0) {
+            changeGroupOfCards();
+            return playerHand.getMostRecentCard();
+        }
+        return null;
+    }
+    
+    public void collectCard(Card card){
+        playedHand.addCard(card);
+    }
+    
+    public void collectCards(GroupOfCards groupOfCard){
+        playedHand.addCards(groupOfCard);
+    }
+    
+    public void changeGroupOfCards(){
+        //change pile
+        playerHand.addCards(playedHand); 
+        //clear the pile that already change
+        playedHand.clear();
+    }
+    
+    public int numberOfCards(){
+        return playerHand.getSize() + playedHand.getSize();
+    }
+
+    @Override
+    public String toString() {
+        return getPlayerID() + " WarPlayer{" + "playerHand=" + playerHand + ", playedHand=" + playedHand + '}';
+    }
+    
+}
