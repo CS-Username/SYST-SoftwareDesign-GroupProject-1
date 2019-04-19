6099dca907970df7ecbd35bf2e5865c370056bb3
nawaphanC <46799202+nawaphanC@users.noreply.github.com>
2019-04-19 16:44:16 -0400

    Create WarGameTest.java

---
+++ test/ca/sheridancollege/project/WarGameTest.java
@@ -0,0 +1,15 @@
+package ca.sheridancollege.project;
+
+/**
+ * Test Game
+ * 
+ * @author Nawaphan Chayopathum(Jan)
+ * @modified Cory Salmon
+ */
+public class WarGameTest {
+    
+    public static void main(String[] args) {
+        WarGame testGame = new WarGame();
+        testGame.play();
+    }
+}
