2a157dab2625f8dc0fbef8cbb1db9d1bc616b668
nawaphanC <46799202+nawaphanC@users.noreply.github.com>
2019-04-19 15:48:32 -0400

    Get rid of setup

--- test/ca/sheridancollege/project/StandardTest.java
+++ test/ca/sheridancollege/project/StandardTest.java
@@ -19,22 +19,6 @@ public class StandardTest {
     
     public StandardTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
     /**
      * Test of toString method, of class Standard.
